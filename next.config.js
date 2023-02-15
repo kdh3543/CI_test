1d32af5dd56a4440f01bdfd57078dc5acf87f096
kdh3543 <rlaehdgus3543@naver.com>
2023-02-15 11:48:03 +0900

    production 배포 테스트

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   experimental: { esmExternals: true },
+  assetPrefix: isProd ? "https://master.dgnto816nv0gi.amplifyapp.com/" : "",
 };
 
 module.exports = nextConfig;
